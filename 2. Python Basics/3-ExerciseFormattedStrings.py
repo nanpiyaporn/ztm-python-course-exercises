9eaa60bf40326638387aa76b95df09484b84ff60
Piyaporn Puangprasert <dngooddeals@gmail.com>
2025-07-13 14:59:09 -0400

    day one 0-1

--- 2. Python Basics/3-ExerciseFormattedStrings.py
+++ 2. Python Basics/3-ExerciseFormattedStrings.py
@@ -10,6 +10,9 @@ print("Hello {name}, your balance is {amount}.".format(name="Cindy", amount=50))
 print("Hello {0}, your balance is {amount}.".format("Cindy", amount=50))
 
 # 2 How would you write this using f-string? (Scroll down for answer)
+name="Mindy"
+age=60
+print(f'Hello {name}, your balance is {age}.')
 
 
 
--- 2. Python Basics/4-ExerciseStringIndexes.py
+++ 2. Python Basics/4-ExerciseStringIndexes.py
@@ -1,9 +1,9 @@
 #Guess the output of each print statement before you click RUN!
 python = 'I am PYHTON'
 
-print(python[1:4])
-print(python[1:])
-print(python[:])
+print(python[1:4]) # ' am '
+print(python[1:]) # ' am PYHTON'
+print(python[:]) # 
 print(python[1:100])
 print(python[-1])
 print(python[-4])
